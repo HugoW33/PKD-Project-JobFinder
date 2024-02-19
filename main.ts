50a234d0eb54eb7fb95109ad1fd66ec720b6efe9
Hugo Wass <hugo.wass@mac.com>
2024-02-19 12:47:13 +0100

    Lade till test kod för axios

--- main.ts
+++ main.ts
@@ -1,1 +1,12 @@
 import axios from "axios";
+
+//console.log(axios.isCancel('something'));
+
+axios({
+    method: 'get', 
+    url: 'https://arbetsformedlingen.se/platsbanken/annonser?q=sjuksköterska',
+    responseType: 'stream'
+})
+.then(response => {
+        console.log(response);
+});
