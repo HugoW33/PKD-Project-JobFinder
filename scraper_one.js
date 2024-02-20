375993877aca2cb08ad736513bf78e5c940e8d12
erak2781 <akerstromeric@gmail.com>
2024-02-20 16:45:18 +0100

    New version that prints the the propper results

--- scraper_one.js
+++ scraper_one.js
@@ -1,4 +1,40 @@
 "use strict";
+var __awaiter = (this && this.__awaiter) || function (thisArg, _arguments, P, generator) {
+    function adopt(value) { return value instanceof P ? value : new P(function (resolve) { resolve(value); }); }
+    return new (P || (P = Promise))(function (resolve, reject) {
+        function fulfilled(value) { try { step(generator.next(value)); } catch (e) { reject(e); } }
+        function rejected(value) { try { step(generator["throw"](value)); } catch (e) { reject(e); } }
+        function step(result) { result.done ? resolve(result.value) : adopt(result.value).then(fulfilled, rejected); }
+        step((generator = generator.apply(thisArg, _arguments || [])).next());
+    });
+};
+var __generator = (this && this.__generator) || function (thisArg, body) {
+    var _ = { label: 0, sent: function() { if (t[0] & 1) throw t[1]; return t[1]; }, trys: [], ops: [] }, f, y, t, g;
+    return g = { next: verb(0), "throw": verb(1), "return": verb(2) }, typeof Symbol === "function" && (g[Symbol.iterator] = function() { return this; }), g;
+    function verb(n) { return function (v) { return step([n, v]); }; }
+    function step(op) {
+        if (f) throw new TypeError("Generator is already executing.");
+        while (g && (g = 0, op[0] && (_ = 0)), _) try {
+            if (f = 1, y && (t = op[0] & 2 ? y["return"] : op[0] ? y["throw"] || ((t = y["return"]) && t.call(y), 0) : y.next) && !(t = t.call(y, op[1])).done) return t;
+            if (y = 0, t) op = [op[0] & 2, t.value];
+            switch (op[0]) {
+                case 0: case 1: t = op; break;
+                case 4: _.label++; return { value: op[1], done: false };
+                case 5: _.label++; y = op[1]; op = [0]; continue;
+                case 7: op = _.ops.pop(); _.trys.pop(); continue;
+                default:
+                    if (!(t = _.trys, t = t.length > 0 && t[t.length - 1]) && (op[0] === 6 || op[0] === 2)) { _ = 0; continue; }
+                    if (op[0] === 3 && (!t || (op[1] > t[0] && op[1] < t[3]))) { _.label = op[1]; break; }
+                    if (op[0] === 6 && _.label < t[1]) { _.label = t[1]; t = op; break; }
+                    if (t && _.label < t[2]) { _.label = t[2]; _.ops.push(op); break; }
+                    if (t[2]) _.ops.pop();
+                    _.trys.pop(); continue;
+            }
+            op = body.call(thisArg, _);
+        } catch (e) { op = [6, e]; y = 0; } finally { f = t = 0; }
+        if (op[0] & 5) throw op[1]; return { value: op[0] ? op[1] : void 0, done: true };
+    }
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 var promptSync = require("prompt-sync");
 var axios_1 = require("axios");
@@ -6,44 +42,67 @@ var cheerio = require("cheerio");
 //console.log(axios.isCancel('something'));
 var h2Elements = [];
 var prompt = promptSync();
-var job = prompt("vilket jobb: ");
-var result = prompt("vilken vill du börja på: ");
+//const job = prompt("vilket jobb: ")
+//let result = prompt("vilken vill du börja på: ")
 function main(x, y) {
-    if (y === 'alla') {
-        (0, axios_1.default)({
-            method: 'get',
-            url: "https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida".concat(x, "/"),
-        })
-            .then(function (response) {
-            var $ = cheerio.load(response.data);
-            $("div a h3").each(function (index, element) {
-                h2Elements.push($(element).text());
+    return new Promise(function (resolve) {
+        if (y === 'alla') {
+            (0, axios_1.default)({
+                method: 'get',
+                url: "https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida".concat(x, "/"),
+            })
+                .then(function (response) {
+                var $ = cheerio.load(response.data);
+                $("div a h3").each(function (index, element) {
+                    h2Elements.push($(element).text());
+                });
+                resolve();
             });
-            return h2Elements;
-        });
-    }
-    else {
-        (0, axios_1.default)({
-            method: 'get',
-            url: "https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida".concat(x, "/?ks=freetext.").concat(y),
-        })
-            .then(function (response) {
-            var $ = cheerio.load(response.data);
-            $("div a h3").each(function (index, element) {
-                h2Elements.push($(element).text());
+        }
+        else {
+            (0, axios_1.default)({
+                method: 'get',
+                url: "https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida".concat(x, "/?ks=freetext.").concat(y),
+            })
+                .then(function (response) {
+                var $ = cheerio.load(response.data);
+                $("div a h3").each(function (index, element) {
+                    h2Elements.push($(element).text());
+                });
+                resolve();
             });
-            return h2Elements;
+        }
+    });
+}
+function func() {
+    return __awaiter(this, void 0, void 0, function () {
+        var job, result, question, a, w;
+        return __generator(this, function (_a) {
+            switch (_a.label) {
+                case 0:
+                    if (!true) return [3 /*break*/, 5];
+                    job = prompt("vilket jobb: ");
+                    result = prompt("vilken vill du börja på: ");
+                    question = prompt("vilken sida vill du sluta på: ");
+                    a = +result;
+                    _a.label = 1;
+                case 1:
+                    if (!(a <= +question)) return [3 /*break*/, 4];
+                    return [4 /*yield*/, main(result, job)];
+                case 2:
+                    _a.sent();
+                    w = (+result) + 1;
+                    result = w.toString();
+                    _a.label = 3;
+                case 3:
+                    a++;
+                    return [3 /*break*/, 1];
+                case 4:
+                    console.log(h2Elements);
+                    return [3 /*break*/, 5];
+                case 5: return [2 /*return*/];
+            }
         });
-    }
+    });
 }
-while (true) {
-    main(result, job);
-    var question = prompt("vilken sida vill du sluta på: ");
-    for (var a = +result; a <= +question; a++) {
-        main(result, job);
-        var w = (+result) + 1;
-        result = w.toString();
-    }
-    console.log(h2Elements);
-    break;
-}
+func();
--- scraper_one.ts
+++ scraper_one.ts
@@ -8,47 +8,50 @@ import { find } from "cheerio/lib/api/traversing";
 const h2Elements: Array<string> = [];
 
 const prompt = promptSync();
-const job = prompt("vilket jobb: ")
-let result = prompt("vilken vill du börja på: ")
-function main(x:string, y:string): void{
-    if (y === 'alla'){
-        axios({
-            method: 'get', 
-            url: `https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida${x}/`,
-        })
-        .then(response => {
-            const $ = cheerio.load(response.data);
-            $("div a h3").each((index, element) => {
-                h2Elements.push($(element).text());
-        });
-        return h2Elements
-        });
-    }
-    else{
-        axios({
-            method: 'get', 
-            url: `https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida${x}/?ks=freetext.${y}`,
-        })
-        .then(response => {
-            const $ = cheerio.load(response.data);   
-            $("div a h3").each((index, element) => {
-                h2Elements.push($(element).text());
-        });
-        return h2Elements
-        });
-    }
-} 
+//const job = prompt("vilket jobb: ")
+//let result = prompt("vilken vill du börja på: ")
+function main(x:string, y:string): Promise<void>{
+    return new Promise((resolve) =>{
+        if (y === 'alla'){
+            axios({
+                method: 'get', 
+                url: `https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida${x}/`,
+            })
+            .then(response => {
+                const $ = cheerio.load(response.data);
+                $("div a h3").each((index, element) => {
+                    h2Elements.push($(element).text());
+            });
+            resolve()
+            });
+        }
+        else{
+            axios({
+                method: 'get', 
+                url: `https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida${x}/?ks=freetext.${y}`,
+            })  
+            .then(response => {
+                const $ = cheerio.load(response.data);   
+                $("div a h3").each((index, element) => {
+                    h2Elements.push($(element).text());
+            });
+            resolve()
+            });
+        }})
+    } 
 
-
-while(true){
-    main(result,job)
-    let question = prompt("vilken sida vill du sluta på: ")
-    for(let a = +result; a <= +question; a++){
-        main(result,job)
-        const w = (+result) + 1
-        result = w.toString()
+async function func(): Promise<void>{
+    while(true){
+        const job = prompt("vilket jobb: ")
+        let result = prompt("vilken vill du börja på: ")
+        let question = prompt("vilken sida vill du sluta på: ")
+        for(let a = +result; a <= +question; a++){
+            await main(result,job)
+            const w = (+result) + 1
+            result = w.toString()
+        }
+        console.log(h2Elements)
+        break;
     }
-    console.log(h2Elements)
-    break;
 }
-
+func()
