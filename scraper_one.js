353cfc2974f8860d8145010fc3310c193ca58758
erak2781 <akerstromeric@gmail.com>
2024-02-20 14:43:54 +0100

    can now search for diffrent job-tags

--- scraper_one.js
+++ scraper_one.js
@@ -6,18 +6,39 @@ var cheerio = require("cheerio");
 //console.log(axios.isCancel('something'));
 var h2Elements = [];
 var prompt = promptSync();
+var job = prompt("vilket jobb: ");
 var result = prompt("vilken sida: ");
-function main(x) {
-    (0, axios_1.default)({
-        method: 'get',
-        url: "https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida".concat(x, "/"),
-    })
-        .then(function (response) {
-        var $ = cheerio.load(response.data);
-        $("div a h3").each(function (index, element) {
-            h2Elements.push($(element).text());
+function main(x, y) {
+    if (y === 'alla') {
+        (0, axios_1.default)({
+            method: 'get',
+            url: "https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida".concat(x, "/"),
+        })
+            .then(function (response) {
+            var $ = cheerio.load(response.data);
+            $("div a h3").each(function (index, element) {
+                h2Elements.push($(element).text());
+            });
+            console.log(h2Elements);
         });
-        console.log(h2Elements);
-    });
+    }
+    else {
+        (0, axios_1.default)({
+            method: 'get',
+            url: "https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida".concat(x, "/?ks=freetext.").concat(y),
+        })
+            .then(function (response) {
+            var $ = cheerio.load(response.data);
+            $("div a h3").each(function (index, element) {
+                h2Elements.push($(element).text());
+            });
+            if (h2Elements.length === 1) {
+                console.log('inga jobb hittades');
+            }
+            else {
+                console.log(h2Elements);
+            }
+        });
+    }
 }
-main(result);
+main(result, job);
--- scraper_one.ts
+++ scraper_one.ts
@@ -5,23 +5,44 @@ import { attr } from "cheerio/lib/api/attributes";
 import { find } from "cheerio/lib/api/traversing";
 
 //console.log(axios.isCancel('something'));
-const h2Elements: Array<any> = [];
+const h2Elements: Array<string> = [];
 
 const prompt = promptSync();
+const job = prompt("vilket jobb: ")
 const result = prompt("vilken sida: ")
-function main(x:string){
-    axios({
-        method: 'get', 
-        url: `https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida${x}/`,
-    })
-    .then(response => {
-        const $ = cheerio.load(response.data);
-        $("div a h3").each((index, element) => {
-            h2Elements.push($(element).text());
-    });
-        console.log(h2Elements)
-    });
+function main(x:string, y:string): void{
+    if (y === 'alla'){
+        axios({
+            method: 'get', 
+            url: `https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida${x}/`,
+        })
+        .then(response => {
+            const $ = cheerio.load(response.data);
+            $("div a h3").each((index, element) => {
+                h2Elements.push($(element).text());
+        });
+            console.log(h2Elements)
+        });
+    }
+    else{
+        axios({
+            method: 'get', 
+            url: `https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida${x}/?ks=freetext.${y}`,
+        })
+        .then(response => {
+            const $ = cheerio.load(response.data);   
+            $("div a h3").each((index, element) => {
+                h2Elements.push($(element).text());
+        });
+        if(h2Elements.length === 1){
+            console.log('inga jobb hittades')
+        }else{
+            console.log(h2Elements)
+        }
+        });
+    }
 } 
 
-main(result)
+
+main(result, job)
 
