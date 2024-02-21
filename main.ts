b9e1b17acfe3defcf559bb5643d5f5ff7f558a50
Hugo Wass <hugo.wass@mac.com>
2024-02-21 12:54:49 +0100

    Changed main.ts to new functionallity

--- main.ts
+++ main.ts
@@ -1,27 +1,5 @@
-import * as promptSync  from 'prompt-sync'
-import axios from "axios";
-import * as cheerio from "cheerio"
-import { attr } from "cheerio/lib/api/attributes";
-import { find } from "cheerio/lib/api/traversing";
+import { promise } from "selenium-webdriver";
+import { RunFunc, type JobbLst, JobbElements, main } from "./scraper_one";
+import * as promptSync from 'prompt-sync'
 
-//console.log(axios.isCancel('something'));
-const h2Elements: Array<any> = [];
-
-const prompt = promptSync();
-const result = prompt("vilken sida: ")
-function main(x:string){
-    axios({
-        method: 'get', 
-        url: `https://jobb.blocket.se/lediga-jobb-i-hela-sverige/sida${x}/`,
-    })
-    .then(response => {
-        const $ = cheerio.load(response.data);
-        $("div a h3").each((index, element) => {
-            h2Elements.push($(element).text());
-    });
-        console.log(h2Elements)
-    });
-} 
-
-main(result)
-
+const JobbArr: Array<string> = [];
